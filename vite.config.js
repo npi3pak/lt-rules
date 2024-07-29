61417a233fcd068919b349246620f8060f612b48
Ivan Korablin <ivan.korablin@sensus.aero>
2024-07-29 10:34:42 +0300

    Add update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/lt-rules/", // Ensures the assets are served relative to the docs directory
     build: {
-        outDir: "./docs",
-        emptyOutDir: true,
+        outDir: "docs", // Changes the output directory to docs
+        assetsDir: "", // Optionally, you can set this to an empty string if you want to place assets directly in the docs folder
     },
 });
