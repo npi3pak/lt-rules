3f12127f0472548ae8b4a127113695018062cd2b
Ivan Korablin <ivan.korablin@sensus.aero>
2024-07-30 09:11:23 +0300

    Add normalize

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import "normalize.css";
 import "./App.css";
 import { Nonus } from "./pages/Nonus";
 
 const App = () => {
     return (
-        <>
+        <div className="flex flex-col mt-8 items-center">
             <Nonus />
-        </>
+        </div>
     );
 };
 
